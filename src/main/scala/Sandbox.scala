6a3bf358c8dfc8265a1bf21b84f0c1cb7773d93e
Jun-Honda-0326 <60055417+Jun-Honda-0326@users.noreply.github.com>
2021-03-28 16:36:34 +0900

    create class WorkersAssignToCompanyService

---
+++ src/main/scala/Sandbox.scala
@@ -0,0 +1,56 @@
+import scala.concurrent.{ Future, ExecutionContext }
+import scala.concurrent.ExecutionContext.Implicits.global
+import cats.data.EitherT
+import cats.implicits._
+
+import java.time.LocalDate
+
+case class Worker(name: String)
+
+trait FetchWorkerOnWorkDay {
+  import FetchWorkerOnWorkDay._
+  def excute(date: LocalDate): Future[Either[Err, List[Worker]]]
+}
+
+object FetchWorkerOnWorkDay {
+  sealed trait Err
+  object Err {
+    case object NoWorkers extends Err
+  }
+}
+
+trait FetchCompanyRequest {
+  import FetchCompanyRequest._
+  def excute(companyId: Long, date: LocalDate)
+}
+case class CompanyRequest(companyId: Long, date: LocalDate, numberOfPeople: Int, content: String)
+object FetchCompanyRequest {
+  sealed trait Err
+  object Err {
+    case object CompanyNotFound extends Err
+    case object IllegalDate extends Err
+    case object NotRecruiting extends Err
+  }
+}
+
+
+class WorkerAssignToCompanyService(
+  fetchWorkersOnDay: FetchWorkerOnWorkDay,
+  fetchCompanyRequest: FetchCompanyRequest
+) {
+  import WorkerAssignToCompanyService._
+  def excute(date: LocalDate, companyId: Long): Future[Either[Err, Result]] = {
+    val e: EitherT[Future, Err, Result] = for {
+      companyRequest <- EitherT(fetchCompanyRequest.excute(companyId, date)).leftMap {
+        case FetchCompanyRequest.Err.CompanyNotFound => Err.CompanyNotFound
+        case FetchCompanyRequest.Err.IllegalDate     => Err.IllegalDate
+        case FetchCompanyRequest.Err.NotRecruiting   => Err.NotRecruiting
+      }
+      workersOnWorkDay <- EitherT(fetchWorkersOnDay.excute(date)).leftMap {
+        case FetchWorkerOnWorkDay.Err.NoWorkers => Err.NoWorkers
+      }
+      assignedWorkers <- EitherT(Future.successful(assignWorkers(workersOnWorkDay, companyRequest.numberOfPeople)))
+    } yield Result(companyRequest, assignedWorkers)
+    e.value
+  }
+}
